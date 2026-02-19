9a0160c8d9622824e6c20c5a0724573701fcbd9a
szmrki <suzumura.kei.x5@elms.hokudai.ac.jp>
2026-02-19 10:44:53 +0900

    test

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -3,7 +3,7 @@ import os
 import pytest
 from pathlib import Path
 
-# Add the project root to sys.path so we can import modules
+# Add the project root to sys.path so we can import modules 
 sys.path.append(str(Path(__file__).parent.parent))
 
 def test_imports():
